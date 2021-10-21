a499a5e8340d76347102afe5784c8096b3455857
aarenstade <aarenstade@gmail.com>
2021-10-21 13:17:04 -0700

    misc

---
+++ pages/test.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Canvas from "../components/Canvas";
+import styles from "../styles/Home.module.css";
+
+import { Rnd } from "react-rnd";
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <div style={{ width: "100px", backgroundColor: "red", height: "auto" }}>
+        <p>
+          sdkgnsdkjgnasdlgjknagljknsdfljndglianregipandgpiansdgoipansdgpiunergpinwergipuebrgiupbergiopabsdfoiandg[oiansgd
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
