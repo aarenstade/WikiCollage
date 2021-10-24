5b4f2beda1f2fead0f749525574ff38d4c9e3ae9
aarenstade <aarenstade@gmail.com>
2021-10-24 13:24:20 -0700

    fetching latest submission

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,23 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { DATABASE_REF } from "../client/firebase";
+import { get } from "firebase/database";
 import Canvas from "../components/Canvas";
 import styles from "../styles/Home.module.css";
+import MainView from "../views/MainView";
 
 const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <Canvas />
-    </div>
-  );
+  const [latestSubmission, setLatestSubmission] = useState(null);
+
+  useEffect(() => {
+    get(DATABASE_REF(`/latest_submission/`))
+      .then((res) => res.exists() && setLatestSubmission(res.val()))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return <div className={styles.container}>{latestSubmission ? <MainView /> : <p>Loading...</p>}</div>;
 };
 
 export default Home;
