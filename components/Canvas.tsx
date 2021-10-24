6f69bb6ddb37f926583d0e3a93dfc48a049e6c07
aarenstade <aarenstade@gmail.com>
2021-10-24 13:24:50 -0700

    moved context higher up tree

--- components/Canvas.tsx
+++ components/Canvas.tsx
@@ -4,16 +4,14 @@ import CanvasAdd from "./CanvasAdd";
 import { useRecoilState } from "recoil";
 import { ElementListState, SelectedElementIdState } from "../data/atoms";
 import CanvasElement from "./CanvasElement";
+import { Submission } from "../types/canvas";
+import { useCanvas } from "../views/MainView";
 
-interface CanvasContextInterface {
-  ref: MutableRefObject<HTMLCanvasElement | null>;
-}
-
-const CanvasContext = createContext<CanvasContextInterface | null>(null);
-
-const useFullCanvas = () => {
+export const useFullCanvas = () => {
   const ref = useRef<HTMLCanvasElement | null>(null);
   const [windowDimensions, setWindowDimensions] = useState({ width: 500, height: 500 });
+  const [latestSubmission, setLatestSubmission] = useState<Submission | null>(null);
+
   const updateWidthAndHeight = () => setWindowDimensions({ width: window.innerWidth, height: window.innerHeight });
 
   const resizeCanvas = (w: number, h: number) => {
@@ -44,12 +42,11 @@ const useFullCanvas = () => {
     if (canvas) resizeCanvas(windowDimensions.width, windowDimensions.height);
   }, [windowDimensions]);
 
-  return { ref };
+  return { ref, latestSubmission };
 };
-export const useCanvas = () => useContext(CanvasContext);
 
 const Canvas = () => {
-  const canvas = useFullCanvas();
+  const canvas = useCanvas();
   const [modify, setModify] = useState({ active: false, x: 0, y: 0 });
   const [elementsList, setElementsList] = useRecoilState(ElementListState);
   const [selectedId, setSelectedId] = useRecoilState(SelectedElementIdState);
@@ -58,7 +55,7 @@ const Canvas = () => {
     if (modify.active) {
       setModify({ ...modify, active: false });
     } else {
-      const canv = canvas.ref.current;
+      const canv = canvas?.ref.current;
       if (canv) {
         const rect = canv.getBoundingClientRect();
         setModify({ active: true, x: e.clientX - rect.left, y: e.clientY - rect.top });
@@ -75,14 +72,13 @@ const Canvas = () => {
   };
 
   return (
-    <CanvasContext.Provider value={canvas}>
+    <div>
       {modify.active && <CanvasAdd modify={modify} onAdd={() => setModify({ ...modify, active: false })} />}
-      {elementsList.map((element: CanvasElementItem, i: number) => {
-        // TODO create selectable editable elements
-        return <CanvasElement key={i} id={i} element={element} onSave={(e) => saveElement(i, e)} />;
-      })}
-      <canvas ref={canvas.ref} style={{ zIndex: 3 }} onClick={handleCanvasClick} />
-    </CanvasContext.Provider>
+      {elementsList.map((element: CanvasElementItem, i: number) => (
+        <CanvasElement key={i} id={i} element={element} onSave={(e) => saveElement(i, e)} />
+      ))}
+      <canvas ref={canvas?.ref} style={{ zIndex: 3 }} onClick={handleCanvasClick} />
+    </div>
   );
 };
 
