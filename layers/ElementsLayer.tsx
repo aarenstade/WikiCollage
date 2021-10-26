5783f1a2fddc2a845c8e889a247cdf04eda76206
aarenstade <aarenstade@gmail.com>
2021-10-26 13:37:03 -0700

    control layers

---
+++ layers/ElementsLayer.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { useRecoilState } from "recoil";
+import { ElementListState, SelectedElementIdState } from "../data/atoms";
+import { CanvasElementItem } from "../types/elements";
+
+import CanvasElement from "../components/CanvasElement";
+
+import styles from "../styles/layers.module.css";
+import useFullCanvas from "../hooks/useFullCanvas";
+import CanvasAdd from "../components/CanvasAdd";
+
+const ElementsLayer = () => {
+  const canvas = useFullCanvas();
+
+  const [elementsList, setElementsList] = useRecoilState(ElementListState);
+  const [selectedId, setSelectedId] = useRecoilState(SelectedElementIdState);
+  const [modify, setModify] = useState({ active: false, x: 0, y: 0 });
+
+  const handleClick = (e: React.MouseEvent) => {
+    console.log("handleClick");
+    if (modify.active) {
+      setModify({ ...modify, active: false });
+    } else {
+      const { screenLeft, screenTop } = window;
+      setModify({ active: true, x: e.clientX, y: e.clientY });
+      setSelectedId(null);
+    }
+  };
+
+  const saveElement = (i: number, e: CanvasElementItem) => {
+    let newElements = [...elementsList];
+    newElements[i] = e;
+    setElementsList(newElements);
+    setSelectedId(null);
+  };
+
+  return (
+    <div className={styles.full} id="elements-root" style={{ zIndex: 2 }}>
+      {modify.active && <CanvasAdd modify={modify} onAdd={() => setModify({ ...modify, active: false })} />}
+      {elementsList.map((element: CanvasElementItem, i: number) => (
+        <CanvasElement key={i} id={i} element={element} onSave={(e) => saveElement(i, e)} />
+      ))}
+      <canvas ref={canvas?.ref} onClick={handleClick} />
+    </div>
+  );
+};
+
+export default ElementsLayer;
---
+++ layers/MuralLayer.tsx
@@ -0,0 +1,30 @@
+/* eslint-disable @next/next/no-img-element */
+// this layer loads the underlying mural image
+
+import { VFC } from "react";
+import useViewControl from "../services/ViewControl";
+
+import styles from "../styles/layers.module.css";
+
+interface Props {
+  mural: string;
+}
+
+const MuralLayer: VFC<Props> = ({ mural }) => {
+  const viewControl = useViewControl();
+  const dimension = 10000;
+
+  // TODO (optimization)
+
+  // when view control changes, calculate set of visible squares of mural
+  // select those images to request and display only
+  // (for now were just loading the entire image)
+
+  return (
+    <div className={styles.full}>
+      <img src={mural} alt="mural" style={{ zIndex: 6, width: `${dimension * viewControl.view.scale}px` }} />
+    </div>
+  );
+};
+
+export default MuralLayer;
