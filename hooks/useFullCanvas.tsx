0ad5d0d5e67731f9daeb01e6d852d31b2a597873
aarenstade <aarenstade@gmail.com>
2021-10-26 13:36:41 -0700

    useFullCanvas hook

---
+++ hooks/useFullCanvas.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useRef, useState } from "react";
+
+export const useFullCanvas = () => {
+  const ref = useRef<HTMLCanvasElement | null>(null);
+  const [windowDimensions, setWindowDimensions] = useState({ width: 500, height: 500 });
+
+  const updateWidthAndHeight = () => setWindowDimensions({ width: window.innerWidth, height: window.innerHeight });
+
+  const resizeCanvas = (w: number, h: number) => {
+    const _canvas = ref.current;
+    var tempCanvas = document.createElement("canvas");
+    var tempContext = tempCanvas.getContext("2d");
+    if (tempContext && _canvas) {
+      tempCanvas.width = w;
+      tempCanvas.height = h;
+      tempContext.fillStyle = "transparent";
+      tempContext.fillRect(0, 0, w, h);
+      tempContext.drawImage(_canvas, 0, 0);
+      _canvas.width = w;
+      _canvas.height = h;
+      const _ctx = _canvas.getContext("2d");
+      _ctx!.drawImage(tempCanvas, 0, 0);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateWidthAndHeight);
+    setWindowDimensions({ width: window.innerWidth, height: window.innerHeight });
+    return () => window.removeEventListener("resize", updateWidthAndHeight);
+  }, []);
+
+  useEffect(() => {
+    const canvas = ref.current;
+    if (canvas) resizeCanvas(windowDimensions.width, windowDimensions.height);
+  }, [windowDimensions]);
+
+  return { ref };
+};
+
+export default useFullCanvas;
