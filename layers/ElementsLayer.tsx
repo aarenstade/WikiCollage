ea34d1ec54f99906ffcffdf5efa8111b2e96fa6d
aarenstade <aarenstade@gmail.com>
2021-10-27 13:51:10 -0700

    sizing

--- layers/ElementsLayer.tsx
+++ layers/ElementsLayer.tsx
@@ -8,8 +8,10 @@ import CanvasElement from "../components/CanvasElement";
 import styles from "../styles/layers.module.css";
 import useFullCanvas from "../hooks/useFullCanvas";
 import CanvasAdd from "../components/CanvasAdd";
+import useViewControl from "../services/ViewControl";
 
 const ElementsLayer = () => {
+  const view = useViewControl();
   const canvas = useFullCanvas();
 
   const [elementsList, setElementsList] = useRecoilState(ElementListState);
@@ -21,7 +23,6 @@ const ElementsLayer = () => {
     if (modify.active) {
       setModify({ ...modify, active: false });
     } else {
-      const { screenLeft, screenTop } = window;
       setModify({ active: true, x: e.clientX, y: e.clientY });
       setSelectedId(null);
     }
@@ -35,7 +36,16 @@ const ElementsLayer = () => {
   };
 
   return (
-    <div className={styles.full} id="elements-root" style={{ zIndex: 2 }}>
+    <div
+      className={styles.full}
+      id="elements-root"
+      style={{
+        zIndex: 2,
+        width: 10000 * view.view.scale,
+        height: 10000 * view.view.scale,
+        backgroundColor: "green",
+      }}
+    >
       {modify.active && <CanvasAdd modify={modify} onAdd={() => setModify({ ...modify, active: false })} />}
       {elementsList.map((element: CanvasElementItem, i: number) => (
         <CanvasElement key={i} id={i} element={element} onSave={(e) => saveElement(i, e)} />
