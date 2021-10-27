4f9798358a67395409ddeca6806055f746ef05e8
aarenstade <aarenstade@gmail.com>
2021-10-27 13:50:12 -0700

    moved submission handling to menu layer

--- layers/MenuLayer.tsx
+++ layers/MenuLayer.tsx
@@ -1,11 +1,82 @@
-import { VFC } from "react";
+import React, { useState, VFC } from "react";
 import styles from "../styles/layers.module.css";
 import useViewControl from "../services/ViewControl";
+import { useRecoilState } from "recoil";
+import { SelectedElementIdState } from "../data/atoms";
+import html2canvas from "html2canvas";
 
 //shows tooltip dialogs
 
-const MenuLayer: VFC = () => {
+interface MenuLayerProps {}
+
+const MenuLayer: VFC<MenuLayerProps> = ({}) => {
   const view = useViewControl();
+  const [ready, setReady] = useState(false);
+  const [processing, setProcessing] = useState(false);
+
+  const [selectedId, setSelectedId] = useRecoilState(SelectedElementIdState);
+
+  const handleReady = () => {
+    setReady(true);
+    // view.setScale(1);
+    setSelectedId(null);
+  };
+
+  // TODO: cant create single layer image of 10,000 x 10,000 (exceeded browser canvas size support)
+  // need way to break html element up, or just export at lower resolution and upscale?
+
+  // options:
+  // RENDER SERVERSIDE - send html node to api endpoint and render serverside?
+  // UPSCALE - export at lower resolution and upscale?
+  // CHANGE DEFAULT SIZE - change default mural size to something better supported by all browsers
+  // CUT AND REASSEMBLE - break html element up into smaller sections (hard)
+
+  // TODO: setup serverside endpoint to recieve html node
+  // see if we can render html2canvas serverside
+
+  const handleSubmit = async (e: React.MouseEvent) => {
+    e.preventDefault();
+    setProcessing(true);
+    const elementsRoot = document.getElementById("elements-root");
+    if (elementsRoot) {
+      console.log(`width: ${elementsRoot.style["width"]}`);
+      console.log(`height: ${elementsRoot.style["height"]}`);
+      // elementsRoot.style["backgroundColor"] = "black";
+
+      const elementsRes = await html2canvas(elementsRoot, { width: 10000, height: 10000 });
+      const elements = elementsRes.toDataURL("image/png");
+      console.log({ elements });
+    }
+    // if (elementsRoot && canvas.ref.current) {
+    //   elementsRoot.style["backgroundColor"] = "black";
+    //   elementsRoot.style["width"] = `${canvas.ref.current.width}px`;
+    //   elementsRoot.style["height"] = `${canvas.ref.current.height}px`;
+
+    //   const canvasRes = await html2canvas(elementsRoot);
+    //   const base64 = canvasRes.toDataURL("image/png");
+    //   console.log({ base64 });
+    //   const bytes = convertBase64ToBytes(base64);
+
+    //   // handle upload and database submission
+    //   const layer_id = v4();
+    //   const filename = `layer_${layer_id}.jpg`;
+
+    //   const storagePath = `/layers/${filename}`;
+    //   await uploadImage(bytes, storagePath);
+    //   const layer = await getDownloadURL(STORAGE_REF(storagePath));
+
+    //   console.log({ layer });
+
+    //   await set(DATABASE_REF(`/latest_submission`), {
+    //     layer,
+    //     name: layer_id,
+    //     timestamp: Date.now(),
+    //     mural: "TODO",
+    //   });
+
+    //   setProcessing(false);
+    // }
+  };
 
   return (
     <div className={styles.menuLayer}>
@@ -15,6 +86,16 @@ const MenuLayer: VFC = () => {
       <button style={{ top: "30px" }} onClick={() => view.zoomOut()}>
         Zoom Out
       </button>
+      {!ready && (
+        <button style={{ bottom: 0, right: 0 }} onClick={handleReady}>
+          Prepare for Submission
+        </button>
+      )}
+      {ready && (
+        <button style={{ bottom: 0, right: 0 }} onClick={handleSubmit}>
+          {processing ? "..." : "Submit"}
+        </button>
+      )}
     </div>
   );
 };
--- views/GlobalCollabView.tsx
+++ views/GlobalCollabView.tsx
@@ -1,4 +1,4 @@
-import { useState, VFC } from "react";
+import React, { useState, VFC } from "react";
 import { useRecoilState } from "recoil";
 import { SelectedElementIdState } from "../data/atoms";
 
@@ -8,65 +8,13 @@ import MenuLayer from "../layers/MenuLayer";
 
 import html2canvas from "html2canvas";
 import { Submission } from "../types/canvas";
+import useViewControl from "../services/ViewControl";
 
 interface Props {
   submission: Submission;
 }
 
 const GlobalCollabView: VFC<Props> = ({ submission }) => {
-  const [processing, setProcessing] = useState(false);
-  const [selectedId, setSelectedId] = useRecoilState(SelectedElementIdState);
-
-  const handleSubmit = async () => {
-    setProcessing(true);
-    setSelectedId(null);
-    const elementsRoot = document.getElementById("elements-root");
-    const muralRoot = document.getElementById("mural-root");
-    if (elementsRoot && muralRoot) {
-      elementsRoot.style["backgroundColor"] = "black";
-      elementsRoot.style["width"] = `${window.innerWidth}px`;
-      elementsRoot.style["height"] = `${window.innerHeight}px`;
-
-      const muralRes = await html2canvas(muralRoot);
-      const elementsRes = await html2canvas(elementsRoot);
-
-      const mural = muralRes.toDataURL("image/png");
-      const elements = elementsRes.toDataURL("image/png");
-
-      console.log({ mural });
-      console.log({ elements });
-    }
-    // if (elementsRoot && canvas.ref.current) {
-    //   elementsRoot.style["backgroundColor"] = "black";
-    //   elementsRoot.style["width"] = `${canvas.ref.current.width}px`;
-    //   elementsRoot.style["height"] = `${canvas.ref.current.height}px`;
-
-    //   const canvasRes = await html2canvas(elementsRoot);
-    //   const base64 = canvasRes.toDataURL("image/png");
-    //   console.log({ base64 });
-    //   const bytes = convertBase64ToBytes(base64);
-
-    //   // handle upload and database submission
-    //   const layer_id = v4();
-    //   const filename = `layer_${layer_id}.jpg`;
-
-    //   const storagePath = `/layers/${filename}`;
-    //   await uploadImage(bytes, storagePath);
-    //   const layer = await getDownloadURL(STORAGE_REF(storagePath));
-
-    //   console.log({ layer });
-
-    //   await set(DATABASE_REF(`/latest_submission`), {
-    //     layer,
-    //     name: layer_id,
-    //     timestamp: Date.now(),
-    //     mural: "TODO",
-    //   });
-
-    //   setProcessing(false);
-    // }
-  };
-
   return (
     <div>
       <MenuLayer />
