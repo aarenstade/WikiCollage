7aa5d7dbfeb9cd04da7f95cea57080766345c620
aarenstade <aarenstade@gmail.com>
2021-10-27 13:51:22 -0700

    zoom in and out control

--- services/ViewControl.tsx
+++ services/ViewControl.tsx
@@ -6,20 +6,39 @@ interface ViewControlHook {
   view: ViewControl;
   zoomIn: () => void;
   zoomOut: () => void;
+  setScale: (n: number) => void;
 }
 
 const useViewControl = (): ViewControlHook => {
   const [viewControl, setViewControl] = useRecoilState(ViewControlState);
   const scaleInc = 0.1;
 
-  const zoomIn = () => viewControl.scale < 1 && setViewControl({ ...viewControl, scale: viewControl.scale + scaleInc });
-  const zoomOut = () =>
-    viewControl.scale > 0.1 && setViewControl({ ...viewControl, scale: viewControl.scale - scaleInc });
+  const zoomIn = () => {
+    if (viewControl.scale >= 0.1) {
+      setViewControl({ ...viewControl, scale: viewControl.scale + scaleInc });
+    } else {
+      setViewControl({ ...viewControl, scale: viewControl.scale + 0.02 });
+    }
+  };
+  const zoomOut = () => {
+    if (viewControl.scale > 0.1) {
+      setViewControl({ ...viewControl, scale: viewControl.scale - scaleInc });
+    } else {
+      if (viewControl.scale <= 0.05) {
+        setViewControl({ ...viewControl, scale: 0.05 });
+      } else {
+        setViewControl({ ...viewControl, scale: viewControl.scale - 0.02 });
+      }
+    }
+  };
+
+  const setScale = (n: number) => setViewControl({ ...viewControl, scale: n });
 
   return {
     view: viewControl,
     zoomIn,
     zoomOut,
+    setScale,
   };
 };
 
