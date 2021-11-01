b2aa95fed7d896d30cef3ad81664f79cd782b960
aarenstade <aarenstade@gmail.com>
2021-11-01 15:57:45 -0700

    handleSubmit prepare elements for merge-mural api

--- layers/MenuLayer.tsx
+++ layers/MenuLayer.tsx
@@ -1,8 +1,8 @@
 import React, { useState, VFC } from "react";
 import styles from "../styles/layers.module.css";
 import useViewControl from "../hooks/useViewControl";
-import { useRecoilState } from "recoil";
-import { SelectedElementIdState } from "../data/atoms";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { ElementListState, SelectedElementIdState } from "../data/atoms";
 import html2canvas from "html2canvas";
 import { convertAllHtmlImagesToBase64, convertBase64ToBytes, uploadImage } from "../image-utils";
 import { v4 } from "uuid";
@@ -13,6 +13,14 @@ import Popup from "../components/Popup";
 import axios from "axios";
 import { BASE_URL } from "../config";
 
+interface ElementToEmbed {
+  uri: string;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}
+
 interface MenuLayerProps {}
 
 const MenuLayer: VFC<MenuLayerProps> = ({}) => {
@@ -21,6 +29,8 @@ const MenuLayer: VFC<MenuLayerProps> = ({}) => {
   const [processing, setProcessing] = useState(false);
   const [message, setMessage] = useState("");
   const [_, setSelectedId] = useRecoilState(SelectedElementIdState);
+
+  const elementsList = useRecoilValue(ElementListState);
 
   const handleReady = () => {
     setReady(true);
@@ -31,49 +41,70 @@ const MenuLayer: VFC<MenuLayerProps> = ({}) => {
   const handleSubmit = async (e: React.MouseEvent) => {
     e.preventDefault();
 
-    setProcessing(true);
-    const elementsRoot = document.getElementById("elements-root");
+    // upload and assemble element list to merge with previous mural
+    const elementObjects: ElementToEmbed[] = [];
 
-    if (elementsRoot) {
-      setMessage("Gathering Elements...");
-      const elementsRes = await html2canvas(elementsRoot, {
-        allowTaint: true,
-        backgroundColor: null,
-        foreignObjectRendering: true,
-        onclone: async (clone) => await convertAllHtmlImagesToBase64(clone),
-      });
-      const base64 = elementsRes.toDataURL("image/png", 0.25);
-      const bytes = convertBase64ToBytes(base64);
+    for (let i = 0; i < elementsList.length; i++) {
+      const element = elementsList[i];
+      const elementRoot = document.getElementById(`canvas-element-${i}`);
 
-      const layer_id = v4();
-      const filename = `layer_${layer_id}.png`;
-      const storagePath = `/layers/${filename}`;
+      if (elementRoot) {
+        setMessage(`Handling Element ${i + 1}/${elementsList.length}...`);
+        console.log(element.data);
+        // get as canvas element, convert any img tags to base64
 
-      setMessage("Uploading Elements...");
-      await uploadImage(bytes, storagePath);
-      const layer = await getDownloadURL(STORAGE_REF(storagePath));
-      console.log({ layer });
+        console.log({ element });
+        const elementCanvas = await html2canvas(elementRoot, {
+          backgroundColor: null,
+          scale: 1,
+          x: element.x,
+          y: element.y,
+          width: element.width,
+          height: element.height,
+          windowWidth: element.width,
+          windowHeight: element.height,
+          onclone: async (clone) => await convertAllHtmlImagesToBase64(clone),
+        });
+        const base64 = elementCanvas.toDataURL("image/png");
+        const bytes = convertBase64ToBytes(base64);
 
-      setMessage("Merging Layer...");
-      const muralStoragePath = await axios({
-        url: `${BASE_URL}/api/merge-mural`,
-        method: "POST",
-        data: { url: layer },
-      });
+        // upload element content data
+        setMessage(`Uploading Element ${i + 1}/${elementsList.length}...`);
+        const elementId = v4();
+        const storagePath = `/tmp/${elementId}.jpg`;
+        await uploadImage(bytes, storagePath);
+        const uri = await getDownloadURL(STORAGE_REF(storagePath));
 
-      setMessage("Finalizing...");
-      const muralUrl = await getDownloadURL(STORAGE_REF(muralStoragePath.data));
-      await set(DATABASE_REF(`/latest_submission`), {
-        layer,
-        name: layer_id,
-        timestamp: Date.now(),
-        mural: muralUrl,
-      });
-
-      window.open(muralUrl, "_blank");
-      setProcessing(false);
-      setReady(false);
+        // append to objects
+        elementObjects.push({
+          uri,
+          x: element.x,
+          y: element.y,
+          width: element.width,
+          height: element.height,
+        });
+      }
     }
+
+    setMessage(`Merging With Mural...`);
+    // send objects to merge-mural
+    const muralRes = await axios({
+      url: `${BASE_URL}/api/merge-mural`,
+      method: "POST",
+      data: { elements: elementObjects },
+    });
+
+    console.log({ muralRes });
+    const muralUri = muralRes.data;
+    const newMuralUri = await getDownloadURL(STORAGE_REF(muralUri));
+
+    await set(DATABASE_REF(`/latest_submission`), {
+      name: `${v4()}`,
+      timestamp: Date.now(),
+      mural: newMuralUri,
+    });
+
+    console.log({ newMuralUri });
   };
 
   if (ready) {
@@ -93,7 +124,7 @@ const MenuLayer: VFC<MenuLayerProps> = ({}) => {
 
   return (
     <div className={styles.menuLayer}>
-      <p style={{ backgroundColor: "white" }}>Scale: {view.view.scale}</p>
+      <p style={{ backgroundColor: "white", fontSize: "12px", top: "50px", left: 0 }}>Scale: {view.view.scale}</p>
       <button style={{ top: 0 }} onClick={() => view.zoomIn()}>
         Zoom In
       </button>
