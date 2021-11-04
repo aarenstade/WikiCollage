a26bfe08ba42a96eec1ec8ba6630db617262a4d3
aarenstade <aarenstade@gmail.com>
2021-11-03 23:56:15 -0700

    charset

--- pages/api/image-to-base64.ts
+++ pages/api/image-to-base64.ts
@@ -17,10 +17,11 @@ const imageToBase64Handler = async (req: NextApiRequest, res: NextApiResponse) =
   try {
     const url = req.body.url.toString();
     const path = `/tmp/${v4()}.png`;
+
     await downloadImage(url.toString(), path);
 
     const buff = fs.readFileSync(path);
-    const base64 = "data:image/png;base64," + buff.toString("base64");
+    const base64 = "data:image/png;charset=utf-8;base64," + buff.toString("base64");
     fs.rmSync(path);
 
     res.status(200).send(base64);
