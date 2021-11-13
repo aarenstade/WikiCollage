251aaf8324ba14b8918ea50792baba68de855776
aarenstade <aarenstade@gmail.com>
2021-11-13 15:15:57 -0800

    AuthProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
+import AuthProvider from "../services/AuthProvider";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <RecoilRoot>
-      <Component {...pageProps} />
-    </RecoilRoot>
+    <AuthProvider>
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </AuthProvider>
   );
 }
 export default MyApp;
