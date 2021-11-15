26cf393e4cc7e5d652cc062e9bde5b7e77cf4d09
aarenstade <aarenstade@gmail.com>
2021-11-14 19:37:34 -0800

    collage state

--- hooks/useCollage.tsx
+++ hooks/useCollage.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import { CollageState } from "../data/atoms";
 import { useAuth } from "../services/AuthProvider";
 import { fetchAdditions, fetchTopic } from "../services/fetch";
+import { Collage } from "../types/collage";
 import { AdditionItem, TopicItem } from "../types/schemas";
 
 const fetchTopicAndAdditions = async (
@@ -25,29 +28,19 @@ const fetchTopicAndAdditions = async (
   return { topic, addition };
 };
 
-interface Collage {
-  addition: AdditionItem | null;
-  topic: TopicItem | null;
-  loading?: boolean;
-}
-
-const useCollage = (topicString: string): Collage => {
+const useCollage = (topicString?: string): Collage => {
   const auth = useAuth();
-  const [loading, setLoading] = useState(true);
-  const [addition, setAddition] = useState<AdditionItem | null>(null);
-  const [topic, setTopic] = useState<TopicItem | null>(null);
+  const [collage, setCollage] = useRecoilState(CollageState);
 
   useEffect(() => {
-    if (auth?.token) {
-      fetchTopicAndAdditions(auth.token, topicString).then((res) => {
-        setTopic(res.topic);
-        setAddition(res.addition);
-        setLoading(false);
-      });
+    if (auth?.token && topicString) {
+      fetchTopicAndAdditions(auth.token, topicString).then((res) =>
+        setCollage({ topic: res.topic, addition: res.addition, loading: false })
+      );
     }
   }, [auth?.token, topicString]);
 
-  return { topic, addition, loading };
+  return { topic: collage.topic, addition: collage.addition, loading: collage.loading };
 };
 
 export default useCollage;
---
+++ types/collage.ts
@@ -0,0 +1,13 @@
+import { AdditionItem, TopicItem } from "./schemas";
+
+export interface Collage {
+  addition: AdditionItem | null;
+  topic: TopicItem | null;
+  loading?: boolean;
+}
+
+export interface CollageHistory {
+  prev_additions: AdditionItem[] | null;
+  topic_history: TopicItem[] | null;
+  page: number;
+}
