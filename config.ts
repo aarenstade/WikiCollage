4825b6cc3c745a06a725c6e6f6dc4bb6e6fd6def
aarenstade <aarenstade@gmail.com>
2021-12-07 15:40:31 -0800

    config cleanup

--- config.ts
+++ config.ts
@@ -1,16 +1,12 @@
 export const BASE_URL = "http://localhost:3000";
-
-export const MAX_FILE_SIZE = 3072000;
-
 export const MURAL_DIMENSION = 5000;
 
-export const WIKI_COLLAGE_HOME_TOPIC_ID = "61903df19fc7eeec658e618d";
-
-export const DEFAULT_MURAL_BACKGROUND_COLOR = "gray";
-
 export const HOME_TOPIC_NAME = "WikiCollage-Home";
-
-export const WAIT_PERIOD = 10 * 60 * 1000;
-
+export const WIKI_COLLAGE_HOME_TOPIC_ID = "61903df19fc7eeec658e618d";
 export const EMPTY_COLLAGE_URI =
   "https://firebasestorage.googleapis.com/v0/b/visual-collab.appspot.com/o/empty-collage.png?alt=media&token=76160648-0897-481a-95e9-68ab5960f604";
+
+export const WAIT_PERIOD = 10 * 60 * 1000;
+export const MAX_FILE_SIZE = 3072000;
+export const MAX_ELEMENT_PIXEL_AREA = 1000000;
+export const DEFAULT_MURAL_BACKGROUND_COLOR = "gray";
