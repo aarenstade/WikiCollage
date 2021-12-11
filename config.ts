36d8633eb332ce4b148545393390b1c0c65bfe11
aarenstade <aarenstade@gmail.com>
2021-12-10 18:28:48 -0800

    BASE_URL https

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://wikicollage.vercel.app";
+export const BASE_URL = "https://wikicollage.vercel.app";
 export const MURAL_DIMENSION = 5000;
 
 export const HOME_TOPIC_NAME = "WikiCollage-Home";
