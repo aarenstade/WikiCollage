1030176adb9d9620e1cb28475766bd1d7aaef150
aarenstade <aarenstade@gmail.com>
2021-12-10 18:23:41 -0800

    changed base url

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "http://wikicollage.vercel.app";
 export const MURAL_DIMENSION = 5000;
 
 export const HOME_TOPIC_NAME = "WikiCollage-Home";
