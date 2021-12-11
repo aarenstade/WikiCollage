863031debb6af88e468091a17400be30930f85a6
aarenstade <aarenstade@gmail.com>
2021-12-10 20:37:47 -0800

    wikicollage.org

--- config.ts
+++ config.ts
@@ -1,14 +1,15 @@
-export const BASE_URL = "https://wikicollage.vercel.app";
+export const BASE_URL = "https://wikicollage.org";
+// export const BASE_URL = "http://localhost:3000";
 export const MURAL_DIMENSION = 5000;
 
 export const HOME_TOPIC_NAME = "WikiCollage-Home";
 export const WIKI_COLLAGE_HOME_TOPIC_ID = "61903df19fc7eeec658e618d";
 export const EMPTY_COLLAGE_URI =
-  "https://firebasestorage.googleapis.com/v0/b/visual-collab.appspot.com/o/empty-collage.png?alt=media&token=76160648-0897-481a-95e9-68ab5960f604";
+  "https://firebasestorage.googleapis.com/v0/b/visual-collab.appspot.com/o/empty-collage.png?alt=media&token=dc067bc8-0a43-4865-86a6-a3b6c1057f9d";
 
 export const WAIT_PERIOD = 10 * 60 * 1000;
 export const MAX_FILE_SIZE = 3072000;
 export const MAX_ELEMENT_PIXEL_AREA = 1000000;
-export const DEFAULT_MURAL_BACKGROUND_COLOR = "gray";
+export const DEFAULT_MURAL_BACKGROUND_COLOR = "black";
 
 export const GITHUB_URL = "https://github.com/aarenstade/WikiCollage";
