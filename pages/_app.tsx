e280d394e438deb11c6e08d76bf684489caeb96b
aarenstade <aarenstade@gmail.com>
2021-12-10 23:37:41 -0800

    head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,16 @@ import { RecoilRoot } from "recoil";
 import AuthProvider from "../services/FirebaseAuthProvider";
 // import { DAppProvider } from "@usedapp/core";
 import Navbar from "../components/page/Navbar";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
       {/* <DAppProvider config={{ autoConnect: true }}> */}
       <RecoilRoot>
+        <Head>
+          <title>WikiCollage</title>
+        </Head>
         <Navbar />
         <Component {...pageProps} />
       </RecoilRoot>
