f1de773653b3b0786970273309a025777a07bfd4
aarenstade <aarenstade@gmail.com>
2021-12-10 20:34:54 -0800

    loading...

--- views/LoadingSplashView.tsx
+++ views/LoadingSplashView.tsx
@@ -1,5 +1,11 @@
 const LoadingSplashView = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h3 style={{ position: "absolute", top: "var(--navbar-offset)", left: "calc(100vw / 2)", color: "white" }}>
+        Loading...
+      </h3>
+    </div>
+  );
 };
 
 export default LoadingSplashView;
