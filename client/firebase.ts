5943df3a0fbe3684ef3d9509986790a3160c9c8f
aarenstade <aarenstade@gmail.com>
2021-12-10 18:10:43 -0800

    firebaseConfig

--- client/firebase.ts
+++ client/firebase.ts
@@ -5,8 +5,18 @@ import { FirebaseStorage, getStorage, ref as stRef, StorageReference } from "fir
 let app: FirebaseApp;
 let storage: FirebaseStorage;
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyDAkzjzyyWLIdShZmx-djzobXYDVKt-9ok",
+  authDomain: "visual-collab.firebaseapp.com",
+  projectId: "visual-collab",
+  storageBucket: "visual-collab.appspot.com",
+  messagingSenderId: "935348650615",
+  appId: "1:935348650615:web:6e82072afaf837ec9c7c72",
+  measurementId: "G-M1VBQEG23Q",
+};
+
 try {
-  app = initializeApp(JSON.parse(process.env.FIREBASE_CLIENT_CONFIG || "{}"));
+  app = initializeApp(firebaseConfig);
   storage = getStorage(app, "gs://visual-collab.appspot.com");
 } catch (error) {
   console.error({ error });
