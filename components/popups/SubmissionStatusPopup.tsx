27894de7abfd21ff606cfdde2aa4316d04a5d5c8
aarenstade <aarenstade@gmail.com>
2021-12-12 17:46:38 -0800

    loading icon

--- components/popups/SubmissionStatusPopup.tsx
+++ components/popups/SubmissionStatusPopup.tsx
@@ -1,6 +1,10 @@
 /* eslint-disable @next/next/no-img-element */
+import { useRouter } from "next/dist/client/router";
 import { VFC } from "react";
+import useElements from "../../hooks/useElements";
 import { SubmissionStatus } from "../../types/general";
+import { BigButton } from "../Buttons";
+import LoadingIcon from "../LoadingIcon";
 import Popup from "../Popup";
 
 interface Props {
@@ -9,12 +13,21 @@ interface Props {
 }
 
 const SubmissionStatusPopup: VFC<Props> = ({ status, topic }) => {
+  const router = useRouter();
+  const elements = useElements();
   if (status.success) {
     return (
       <Popup noExit onToggle={() => null}>
         <h3 style={{ fontSize: "50px", margin: "0" }}>Thank You</h3>
         <p>Your additions {topic && `to "${topic}"`} was recieved and embedded into the collage.</p>
-        {status.image && <img src={status.image} alt="Collage" width="300px" />}
+        {status.image && <img src={status.image} alt="Collage" width="300px" style={{ marginBottom: "20px" }} />}
+        <BigButton
+          text="Done"
+          onClick={() => {
+            elements.clearElements();
+            router.push("/");
+          }}
+        />
         {/* TODO share links */}
       </Popup>
     );
@@ -37,7 +50,8 @@ const SubmissionStatusPopup: VFC<Props> = ({ status, topic }) => {
             <img src={status.image} alt="element" width="300px" height="auto" />
           ) : (
             //   TODO cool loading icon
-            <div style={{ backgroundColor: "lightgray", width: "300px", height: "300px" }} />
+            <LoadingIcon />
+            // <div style={{ backgroundColor: "lightgray", width: "300px", height: "300px" }} />
           )}
         </div>
       )}
