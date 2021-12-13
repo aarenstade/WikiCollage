8009f1338859bdc298fad480d06699b6e2143e6e
aarenstade <aarenstade@gmail.com>
2021-12-12 18:27:04 -0800

    build single addition image client side

--- hooks/useSubmitHandler.tsx
+++ hooks/useSubmitHandler.tsx
@@ -1,14 +1,19 @@
+import { convertAllHtmlImagesToBase64, convertBase64ToBytes, uploadImage } from "../utils/image-utils";
+import { embedNewCollage, insertNewAddition } from "../upload";
+import html2canvas from "html2canvas";
 import { useState } from "react";
 import { useRecoilState } from "recoil";
 import { SelectedElementIdState } from "../data/atoms";
-import { ElementToEmbed } from "../types/elements";
 import { AdditionSubmitFormValues } from "../types/general";
 import { AdditionItem } from "../types/mongodb/schemas";
-import { embedNewMural, insertNewAddition } from "../upload";
 import useAuth from "./useAuth";
 import useCollage from "./useCollage";
 import useElements from "./useElements";
 import useViewControl from "./useViewControl";
+import { MURAL_DIMENSION } from "../config";
+import { v4 } from "uuid";
+import { getDownloadURL } from "firebase/storage";
+import { STORAGE_REF } from "../client/firebase";
 
 interface SubmitHandlerHook {
   message: string;
@@ -44,59 +49,53 @@ const useSubmitHandler = (): SubmitHandlerHook => {
     try {
       if (auth?.firebase?.token) {
         setMessage("Preparing...");
-        const elementsList = [...elements.elements];
-        let elementObjects: ElementToEmbed[] = [];
+        const root = document.getElementById("elements-root");
+        if (root) {
+          setMessage("Handling Images...");
+          const canvas = await html2canvas(root, {
+            backgroundColor: null,
+            scale: 1,
+            width: MURAL_DIMENSION,
+            height: MURAL_DIMENSION,
+            onclone: async (clone) => convertAllHtmlImagesToBase64(clone),
+          });
 
-        // loop through all image objects
-        const imageElements = elementsList.filter((e) => e.type === "image");
-        imageElements.forEach((e) => {
-          const { x, y, width, height, data } = e;
-          elementObjects.push({ uri: data, x, y, width, height });
-        });
+          const base64 = canvas.toDataURL("image/png");
+          const bytes = convertBase64ToBytes(base64);
+          setMessage("Merging Additions...");
 
-        // TODO - once text offset bug is fixed, this uses html2canvas to generate image then uploads
-        // for (let i = 0; i < textElements.length; i++) {
-        //   const base64 = await buildImageFromElement(textElements[i]);
-        //   if (base64) {
-        //     const bytes = convertBase64ToBytes(base64);
-        //     const uri = await uploadImage(bytes, `tmp/${textElements[i].html_id}.png`);
-        //     if (uri) {
-        //       const { x, y, width, height } = textElements[i];
-        //       elementObjects.push({ uri, x, y, width, height });
-        //     }
-        //   }
-        // }
+          const path = `/tmp/${v4()}.png`;
+          await uploadImage(bytes, path);
+          const additionsUrl = await getDownloadURL(STORAGE_REF(path));
+          console.log({ additionsUrl });
 
-        if (elementObjects.length > 0) {
-          setMessage("Embedding...");
-          const mural = await embedNewMural(auth.firebase.token, elementObjects, collage.addition?.url);
-          if (mural) {
-            setMessage("Processing");
-            setLiveImage(mural);
-            let newAddition: AdditionItem = {
-              topic_id: collage.topic?._id,
-              url: mural,
-              name: form.name,
-              email: form.email,
-              description: form.description,
-              address: auth.eth.account || undefined,
-              timestamp: new Date(),
-            };
-            const addition = await insertNewAddition(auth.firebase.token, newAddition, collage.topic);
-            if (addition._id) {
-              setSuccess(true);
-              setMessage("Success!");
+          if (additionsUrl) {
+            const newCollage = await embedNewCollage(auth.firebase.token, additionsUrl, collage.addition?.url);
+            if (newCollage) {
+              let newAddition: AdditionItem = {
+                topic_id: collage.topic?._id,
+                url: newCollage,
+                name: form.name,
+                email: form.email,
+                description: form.description,
+                address: auth.eth.account || undefined,
+                timestamp: new Date(),
+              };
+              const addition = await insertNewAddition(auth.firebase.token, newAddition, collage.topic);
+              if (addition._id) {
+                setLiveImage(newCollage);
+                setSuccess(true);
+                setMessage("Success!");
+              }
             } else {
-              setMessage("ERROR");
-              // TODO better error handling and UI response
+              console.log("collage not created...");
+              // TODO better error handling
             }
           }
-        } else {
-          setMessage("Error processing elements...");
         }
       }
     } catch (error) {
-      setMessage(`Uh oh... an error occurred: ${error}`);
+      console.error({ error });
     }
   };
 
--- upload.ts
+++ upload.ts
@@ -3,35 +3,12 @@ import { getDownloadURL } from "@firebase/storage";
 import { STORAGE_REF } from "./client/firebase";
 import { authPostRequest } from "./client/requests";
 import { BASE_URL } from "./config";
-import { ElementToEmbed } from "./types/elements";
 import { AdditionItem, TopicItem } from "./types/mongodb/schemas";
 
-// export const buildImageFromElement = async (element: CanvasElementItem): Promise<string | null> => {
-//   try {
-//     const elementRoot = document.getElementById(element.html_id);
-//     if (elementRoot) {
-//       elementRoot.style["backgroundColor"] = "blue";
-
-//       const elementCanvas = await html2canvas(elementRoot, {
-//         scale: 1,
-//         width: element.width,
-//         height: element.height,
-//         backgroundColor: null,
-//         onclone: async (clone) => await convertAllHtmlImagesToBase64(clone),
-//       });
-
-//       return elementCanvas.toDataURL("image/png", 0.8);
-//     }
-//     return null;
-//   } catch (error) {
-//     return null;
-//   }
-// };
-
-export const embedNewMural = async (token: string, elementObjects: ElementToEmbed[], collage?: string) => {
+export const embedNewCollage = async (token: string, addition_url: string, collage_url?: string) => {
   try {
-    const muralRes = await authPostRequest(token, `${BASE_URL}/api/merge-mural`, { elements: elementObjects, collage });
-    if (muralRes && muralRes.data) return await getDownloadURL(STORAGE_REF(muralRes.data));
+    const res = await authPostRequest(token, `${BASE_URL}/api/merge-collage`, { addition_url, collage_url });
+    if (res && res.data) return await getDownloadURL(STORAGE_REF(res.data));
   } catch (error) {
     console.error({ error });
   }
