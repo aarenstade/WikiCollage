4719f8d507bbd3f5c4a7e8e0ee185c9212610b10
aarenstade <aarenstade@gmail.com>
2021-12-13 13:26:05 -0800

    wikicollage base url

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
-// export const BASE_URL = "https://www.wikicollage.org";
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://www.wikicollage.org";
+// export const BASE_URL = "http://localhost:3000";
 export const MURAL_DIMENSION = 5000;
 
 export const HOME_TOPIC_NAME = "WikiCollage-Home";
