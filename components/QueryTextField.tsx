b3310a227c740edbe1bcbaaa41bf478b1175582e
aarenstade <aarenstade@gmail.com>
2021-12-14 00:30:27 -0800

    full functionality

--- components/QueryTextField.tsx
+++ components/QueryTextField.tsx
@@ -1,23 +1,34 @@
-import { useEffect, useState, VFC } from "react";
+import { ChangeEvent, KeyboardEventHandler, useEffect, useRef, useState, VFC } from "react";
 import { authGetRequest } from "../client/requests";
 import useAuth from "../hooks/useAuth";
-import { encodeTopicUrlParam } from "../utils/utils";
+import { capitalizeWords, encodeTopicUrlParam } from "../utils/utils";
 import styles from "./QueryTextField.module.css";
 
 interface QueryTextFieldProps {
   endpointUrl: string;
   paramName: string;
+  value?: string;
+  selectOnEnter?: boolean;
+  onChange: (v: string) => void;
   onSelect: (v: string) => void;
 }
 
-const QueryTextField: VFC<QueryTextFieldProps> = ({ endpointUrl, paramName, onSelect }) => {
+const QueryTextField: VFC<QueryTextFieldProps> = ({
+  endpointUrl,
+  paramName,
+  value,
+  selectOnEnter,
+  onChange,
+  onSelect,
+}) => {
   const auth = useAuth();
   const [results, setResults] = useState<string[]>([]);
-  const [value, setValue] = useState("");
+  const [focus, setFocus] = useState(false);
 
   useEffect(() => {
     setTimeout(() => {
-      if (auth.firebase?.token && value) {
+      if (auth.firebase?.token && value && focus) {
+        setResults([]);
         authGetRequest(auth.firebase.token, `${endpointUrl}?${paramName}=${encodeTopicUrlParam(value)}`)
           .then((res) => {
             if (res.data.results) setResults(res.data.results);
@@ -25,12 +36,36 @@ const QueryTextField: VFC<QueryTextFieldProps> = ({ endpointUrl, paramName, onSe
           .catch((err) => console.error({ err }));
       }
     }, 500);
-  }, [auth.firebase?.token, endpointUrl, paramName, value]);
+  }, [auth.firebase?.token, focus, endpointUrl, paramName, value]);
+
+  const selectResult = (i: number) => onSelect(results[i]);
 
   return (
-    <div className={styles.queryTextFieldContainer}>
-      <input name="query-text-field" value={value} onChange={(e) => setValue(e.target.value)} />
-      <ul className={styles.queryTextFieldResults}></ul>
+    <div className={styles.queryTextField}>
+      <div className={styles.queryInputUlStack}>
+        <input
+          onFocus={() => setFocus(true)}
+          type="text"
+          name="query-text-field"
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+          onKeyDown={(e) => e.key === "Enter" && value && selectOnEnter && onSelect(value)}
+          autoComplete="off"
+          onBlur={() =>
+            setTimeout(() => {
+              setResults([]);
+              setFocus(false);
+            }, 200)
+          }
+        />
+        <ul className={styles.queryResultUl}>
+          {results.map((result: string, i: number) => (
+            <li onClick={() => selectResult(i)} key={i}>
+              {capitalizeWords(result)}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
